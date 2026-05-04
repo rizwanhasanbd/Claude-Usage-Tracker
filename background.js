6bdc27ae8095b127457523e92a80ad4c9835a8b4
Rizwan Hasan <rizwanhasan226@gmail.com>
2026-05-05 00:41:06 +0600

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+import './lib/browser-polyfill.min.js';
+import './lib/o200k_base.js';
 import { CONFIG, isElectron, sleep, RawLog, FORCE_DEBUG, containerFetch, addContainerFetchListener, StoredMap, getStorageValue, setStorageValue, removeStorageValue, getOrgStorageKey, sendTabMessage, messageRegistry } from './bg-components/utils.js';
 import { tokenStorageManager, tokenCounter } from './bg-components/tokenManagement.js';
 import { ClaudeAPI, ConversationAPI } from './bg-components/claude-api.js';
